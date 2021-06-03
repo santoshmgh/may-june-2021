23ab5632434cc9d4e10d07ddf93e8168acce20bc
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-06-03 14:47:56 +0530

    Day04

---
+++ day04-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (<div>
+      Hello react
+    </div>);
+  }
+}
+
+export default App;
