35a41ec9977d121aa9865f72a11a4d76e7e03d43
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-06-03 15:52:04 +0530

    Day04

--- day04-app/src/App.js
+++ day04-app/src/App.js
@@ -3,14 +3,24 @@ import {Component} from 'react';
 import Lifecycle from './Lifecycle';
 import Person from './Person';
 
+//Parent
 class App extends Component {
+
+  doSomething(arg) {
+    console.log("Temperature of Chennai is", Math.random() * 50, arg);
+  }
+
+  doSomethingElse(arg) {
+    console.log("Time is", new Date().toLocaleTimeString(), arg);
+  }
+
   render() {
     return (<div>
-      <Person name="Sam" age="12"/>
+      <Person name="Sam" age="12" something={this.doSomething} />
       <hr/>
-      <Person name="Ram" age="23"/>
+      <Person name="Ram" age="23" something={this.doSomethingElse}/>
       <hr/>
-      <Person name="Mary" age="34"/>
+      <Person name="Mary" something={this.doSomething}/>
     </div>);
   }
 }
--- day04-app/src/Person.js
+++ day04-app/src/Person.js
@@ -3,12 +3,20 @@ import { Component } from "react";
 class Person extends Component {
     constructor(props) {
         super(props);
+        this.buttonClicked = this.buttonClicked.bind(this);
+    }
+
+    buttonClicked() {
+        //console.log(new Date().toLocaleTimeString());
+        let message = `From: ` + this.props.name;
+        this.props.something(message);
     }
 
     render() {
         return (<div>
             <h2>Name: {this.props.name}</h2>
             <h4>Age: {this.props.age}</h4>
+            <button onClick={this.buttonClicked}>Click</button>
         </div>);
     }
 }
